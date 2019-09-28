dd7a33390b3cb69d0a0eac904c38d651e000b20d
baba_s <shotababa1206@gmail.com>
2019-09-28 20:18:42 +0900

    first commit

---
+++ Editor/SceneAssetHighlighter.cs
@@ -0,0 +1,33 @@
+﻿using System.IO;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace KoganeUnityLib
+{
+	[InitializeOnLoad]
+	public static class SceneAssetHighlighter
+	{
+		private static readonly Color COLOR = new Color32( 255, 32, 32, 64 );
+		
+		static SceneAssetHighlighter()
+		{
+			EditorApplication.projectWindowItemOnGUI += OnGUI;
+		}
+
+		private static void OnGUI( string guid, Rect selectionRect )
+		{
+			var path = AssetDatabase.GUIDToAssetPath( guid );
+
+			var activeScene = SceneManager.GetActiveScene();
+			var filename    = Path.GetFileNameWithoutExtension( path );
+
+			if ( activeScene.name != filename ) return;
+
+			var color = GUI.color;
+			GUI.color = COLOR;
+			GUI.DrawTexture( selectionRect, EditorGUIUtility.whiteTexture );
+			GUI.color = color;
+		}
+	}
+}
---
+++ README.md
@@ -0,0 +1,21 @@
+# Uni Scene Asset Highlighter
+
+現在アクティブなシーンのアセットをハイライト表示します  
+
+![](https://img.shields.io/badge/Unity-2018.4%2B-red.svg)
+![](https://img.shields.io/badge/.NET-4.x-orange.svg)
+[![](https://img.shields.io/github/license/baba-s/uni-scene-asset-highlighter.svg)](https://github.com/baba-s/uni-scene-asset-highlighter/blob/master/LICENSE)
+
+## インストール
+
+```json
+"com.baba_s.uni-scene-asset-highlighter": "https://github.com/baba-s/uni-scene-asset-highlighter.git",
+```
+
+manifest.json に上記の記述を追加します  
+
+## 使い方
+
+![](https://cdn-ak.f.st-hatena.com/images/fotolife/b/baba_s/20190928/20190928201703.png)
+
+導入するだけで現在アクティブなシーンのアセットがハイライト表示されるようになります  
